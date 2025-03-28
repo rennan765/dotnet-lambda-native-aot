9be6684a11b912b70532c9cfa5e8b302d2dd59f5
Rennan Rezende <rennan765@gmail.com>
2025-03-28 11:51:07 -0300

    chore: pr to develop

---
+++ publish-native-aot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e  # Script fails if has error
+
+# Navigate to folder
+cd src/MaintainUserData
+
+echo 'Restoring dependencies...'
+dotnet restore
+
+echo 'Building...'
+dotnet build --configuration Release --use-current-runtime
+
+echo 'Publishing...'
+dotnet publish --configuration Release --no-restore --use-current-runtime --self-contained true -o ./publish
+
+echo 'Publish succeeded. Showing publish folder content:'
+cd publish
+ls -l
