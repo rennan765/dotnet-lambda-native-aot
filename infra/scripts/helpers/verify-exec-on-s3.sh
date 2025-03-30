9219a4754e78a094749c0e39b3c07e2dd8a7fb6b
Rennan Rezende <rennan765@gmail.com>
2025-03-30 17:36:28 -0300

    fix: add exec on s3

--- infra/scripts/helpers/verify-exec-on-s3.sh
+++ infra/scripts/helpers/verify-exec-on-s3.sh
@@ -8,8 +8,10 @@ is_file_exists=$(aws s3api head-object --bucket $DEPLOY_FUNCTION_BUCKET_NAME --k
 if [ $is_file_exists = "false" ]; then
     echo "Exec file not found. Publishing again..."
 
-    source app/publish-native-aot.sh
+    sh app/publish-native-aot.sh
     aws s3 mv "app/$FUNCTION_FILENAME" s3://$DEPLOY_FUNCTION_BUCKET_NAME/
 
     echo "Publish succeeded, zipped and saved at $DEPLOY_FUNCTION_BUCKET_NAME"
 fi
+
+echo "Exec file found at $DEPLOY_FUNCTION_BUCKET_NAME"
---
+++ infra/scripts/publish-and-move-to-s3.sh
@@ -0,0 +1,8 @@
+chmod +x app/publish-native-aot.sh \
+    infra/scripts/variables/commons.sh \
+    infra/scripts/variables/development.sh \
+    infra/scripts/helpers/verify-exec-on-s3.sh
+
+source infra/scripts/variables/commons.sh
+source infra/scripts/variables/development.sh
+source infra/scripts/helpers/verify-exec-on-s3.sh
