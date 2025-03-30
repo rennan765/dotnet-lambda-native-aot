7a847c26322b39dcf65b9011ffe61c734b6f348f
Rennan Rezende <rennan765@gmail.com>
2025-03-30 17:47:24 -0300

    fix: s3 script

--- infra/scripts/publish-and-move-to-s3.sh
+++ infra/scripts/publish-and-move-to-s3.sh
@@ -3,6 +3,8 @@ chmod +x app/publish-native-aot.sh \
     infra/scripts/variables/development.sh \
     infra/scripts/helpers/verify-exec-on-s3.sh
 
-source infra/scripts/variables/commons.sh
-source infra/scripts/variables/development.sh
-source infra/scripts/helpers/verify-exec-on-s3.sh
+cd infra/scripts/
+
+source variables/commons.sh
+source variables/development.sh
+source helpers/verify-exec-on-s3.sh
