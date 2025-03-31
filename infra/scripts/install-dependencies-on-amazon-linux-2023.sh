523286ba5c6b224900e9bfebad4198018f38d2e9
Rennan Rezende <rennan765@gmail.com>
2025-03-31 20:48:34 -0300

    fix: aws-cli

--- infra/scripts/install-dependencies-on-amazon-linux-2023.sh
+++ infra/scripts/install-dependencies-on-amazon-linux-2023.sh
@@ -2,5 +2,9 @@
 set -e  # Script fails if has error
 
 echo "Installing dependencies..."
-yum install -y gcc glibc glibc-devel dotnet-sdk-8.0 tar gzip zlib-devel
+
+yum install -y gcc glibc glibc-devel \
+    dotnet-sdk-8.0 tar gzip zlib-devel \
+    aws-cli
+
 echo "Finished"
